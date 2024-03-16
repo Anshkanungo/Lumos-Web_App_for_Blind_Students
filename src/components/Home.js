96025f6660c846147598b99989f11cb3c41a5f0d
Amit Singh <iamitsinghthakur123@gmail.com>
2024-03-17 01:08:01 +0530

    added Assets and Home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,23 @@
 import React, { useRef } from "react";
 import { handleSpeak } from "./speechUtils";
+import logo from "../assets/Logo.jpg";
+import "./Home.css";
 
 export default function HomePage() {
   const hasSpokeRef = useRef(false);
-
   const handleSpeakButton = () => {
-    handleSpeak(hasSpokeRef, 'homepage');
+    handleSpeak(hasSpokeRef, "homepage");
   };
 
   return (
     <div className="homepage">
-      <h1>Welcome to HOME page</h1>
-      <button onClick={handleSpeakButton}>Speak</button>
+      <img className="logo" src={logo} alt="logo" />
+      <h1 className="heading">Welcome to Home page </h1>
+      <div className="subject-container">
+        <button className="speak-button" onClick={handleSpeakButton}>
+          Speak
+        </button>
+      </div>
     </div>
   );
 }
