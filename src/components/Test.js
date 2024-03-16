69f27c51efd8c5d94e9aebfae93b9b6238934f38
Amit Singh <iamitsinghthakur123@gmail.com>
2024-03-17 00:08:44 +0530

    Subject.js and Subject.css styling

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function TestDataDisplay({ onDataFetched }) {
-  const [data, setData] = useState('');
+  const [data, setData] = useState("");
 
   useEffect(() => {
-    fetch('test.txt')
-      .then(response => response.text())
-      .then(text => {
+    fetch("test.txt")
+      .then((response) => response.text())
+      .then((text) => {
         setData(text);
         onDataFetched(text); // Call the onDataFetched prop with the fetched data
       })
-      .catch(error => console.error('Error fetching data:', error));
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return null; // Return null since you don't need to render anything in this component
