69fbb72eb494f3ebaa8649e092b59fa91d453984
Amit Singh <iamitsinghthakur123@gmail.com>
2024-03-17 15:56:21 +0530

    9000ms to 15000ms

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function HomePage() {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate("/subject");
-    }, 9000);
+    }, 15000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
